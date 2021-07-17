e337b5672aeda4814b2685585330dba1e874cecf
Jose Villegas <dev@duikerbok.com>
2021-07-17 17:36:58 -0400

    Set up BezierTimeControlView.

--- BezierVisualizer/Source/BezierTimeControlView.swift
+++ BezierVisualizer/Source/BezierTimeControlView.swift
@@ -2,8 +2,18 @@ import SwiftUI
 import UIKit
 
 struct BezierTimeControlView: View {
+  @State private var time = 0.0
+  @State private var isOn = false
+
   var body: some View {
-    Text("Bezier Time Control View")
+    HStack(spacing: 24) {
+      Slider(value: $time) {}
+        .disabled(!isOn)
+        .id(isOn)
+      Toggle(isOn: $isOn, label: { Text("") })
+        .labelsHidden()
+    }
+    .padding(12)
   }
 }
 
@@ -18,38 +28,11 @@ final class BezierTimeControlView_: UIView {
 
   var perform: (Action) -> () = { _ in }
 
-  init() {
-    super.init(frame: .zero)
-
-    let stackView = UIStackView()
-    stackView.spacing = 24
-
-    slider.isEnabled = false
-
-    addSubview(stackView)
-    stackView.addArrangedSubview(slider)
-    stackView.addArrangedSubview(switchView)
-
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    stackView.topAnchor.constraint(equalTo: topAnchor, constant: 12).isActive = true
-    stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -12).isActive = true
-    stackView.leftAnchor.constraint(equalTo: leftAnchor, constant: 12).isActive = true
-    stackView.rightAnchor.constraint(equalTo: rightAnchor, constant: -12).isActive = true
-
-    slider.addTarget(self, action: #selector(sliderDidChange), for: .valueChanged)
-    switchView.addTarget(self, action: #selector(switchDidChange), for: .valueChanged)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
   @objc private func sliderDidChange() {
     perform(.sliderDidChange(slider.value))
   }
 
   @objc private func switchDidChange() {
-    slider.isEnabled = switchView.isOn
     perform(.switchDidChange(isOn: switchView.isOn))
   }
 }
