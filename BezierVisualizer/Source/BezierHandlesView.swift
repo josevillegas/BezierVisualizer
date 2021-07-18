51292249745a73d57923c44f6a7b698d9892cbf8
Jose Villegas <dev@duikerbok.com>
2021-07-17 23:39:18 -0400

    Add handle gesture.

--- BezierVisualizer/Source/BezierHandlesView.swift
+++ BezierVisualizer/Source/BezierHandlesView.swift
@@ -19,7 +19,20 @@ struct BezierHandlesView: View {
 }
 
 struct HandleView: View {
+  @State private var lastOffset: CGSize = .zero
+  @State private var dragOffset: CGSize = .zero
+
   var body: some View {
+    let gesture = DragGesture()
+    .onChanged { gesture in
+      dragOffset = CGSize(
+        width: lastOffset.width + gesture.translation.width,
+        height: lastOffset.height + gesture.translation.height
+      )
+    }
+    .onEnded { gesture in
+      lastOffset = dragOffset
+    }
     ZStack {
       Circle()
         .fill(Color.white)
@@ -28,6 +41,8 @@ struct HandleView: View {
     }
       .frame(width: 10, height: 10)
       .padding(17)
+      .offset(dragOffset)
+      .gesture(gesture)
   }
 }
 
@@ -74,38 +89,5 @@ final class HandleView_: UIView {
   var position: CGPoint {
     get { frame.origin }
     set { frame = CGRect(origin: newValue, size: .zero) }
-  }
-
-  private let layerFrame: CGRect
-  private var dragOrigin: CGPoint?
-
-  init() {
-    let size: CGFloat = 10
-    layerFrame = CGRect(x: -size / 2, y: -size / 2, width: size, height: size)
-    super.init(frame: .zero)
-
-    let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(dragged))
-    addGestureRecognizer(gestureRecognizer)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-
-  @objc private func dragged(_ recognizer: UIPanGestureRecognizer) {
-    switch recognizer.state {
-    case .possible: break
-    case .began: dragOrigin = frame.origin
-    case .changed:
-      guard let dragOrigin = dragOrigin, let superview = superview else { break }
-      let translation = recognizer.translation(in: superview)
-      frame = CGRect(origin: CGPoint(x: dragOrigin.x + translation.x, y: dragOrigin.y + translation.y), size: .zero)
-      didDrag(position)
-    case .ended,
-         .cancelled,
-         .failed:
-      dragOrigin = nil
-    @unknown default: fatalError()
-    }
   }
 }
