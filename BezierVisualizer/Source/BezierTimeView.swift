3d5b80c4d232112c4fe96052dab216a9188456e3
Jose Villegas <dev@duikerbok.com>
2021-07-18 02:39:35 -0400

    Remove PointView file.

--- BezierVisualizer/Source/BezierTimeView.swift
+++ BezierVisualizer/Source/BezierTimeView.swift
@@ -81,3 +81,28 @@ final class BezierTimeView_: UIView {
     layer.path = path.cgPath
   }
 }
+
+final class PointView: UIView {
+  var position: CGPoint {
+    get { frame.origin }
+    set { frame = CGRect(origin: newValue, size: .zero) }
+  }
+
+  init() {
+    super.init(frame: .zero)
+
+    clipsToBounds = false
+
+    let size: CGFloat = 8
+    let sublayer = CALayer()
+    sublayer.frame = CGRect(x: -size / 2, y: -size / 2, width: size, height: size)
+    sublayer.backgroundColor = UIColor(red: 0, green: 0.5, blue: 1, alpha: 1).cgColor
+    sublayer.cornerRadius = size / 2
+
+    layer.addSublayer(sublayer)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
